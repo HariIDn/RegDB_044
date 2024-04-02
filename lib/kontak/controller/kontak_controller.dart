408e16593aad4e84b45036631d5e77412358d345
Aditya Hari_20210140044 <aldifaadit75@gmail.com>
2024-04-02 09:35:12 +0700

    tambah fungsi kontakcontroller

--- lib/kontak/controller/kontak_controller.dart
+++ lib/kontak/controller/kontak_controller.dart
@@ -4,4 +4,8 @@ import 'dart:html';
 
 import 'package:regis_db/kontak/service/kontak_service.dart';
 
-class KontakController {}
+class KontakController {
+  final kontakService = KontakService();
+
+  Future<Map<String, dynamic>> addPerson(Kontak person, File? file) async {}
+}
