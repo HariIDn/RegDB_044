25c83a4d8b07c061f62b7384344e19bd2927d892
Aditya Hari_20210140044 <aldifaadit75@gmail.com>
2024-04-03 04:39:16 +0700

    Menambahkan fungsi untuk mengambil daftar orang dari API

--- lib/kontak/service/kontak_service.dart
+++ lib/kontak/service/kontak_service.dart
@@ -1,4 +1,5 @@
-import 'dart:html';
+import 'dart:convert';
+import 'dart:io';
 
 import 'package:http/http.dart' as http;
 
@@ -25,5 +26,22 @@ class KontakService {
     }
 
     return await http.Response.fromStream(await request.send());
+  }
+
+  Future<List<dynamic>> fetchPeople() async {
+    var response = await http.get(
+        getUri(
+          endpoint,
+        ),
+        headers: {
+          "Accept": "application/json",
+        });
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> decodeResponse = json.decode(response.body);
+      return decodeResponse['people'];
+    } else {
+      throw Exception('Failed to load people: ${response.reasonPhrase}');
+    }
   }
 }
